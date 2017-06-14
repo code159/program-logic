46c5874e5f41d5f617839dfcd1c2997e25a2e859
liyubin <409901403@qq.com>
2017-06-14 17:21:37 +0800

    Update InterruptReadDemo.java
    用FileInputStream对象代替InputStream，close方法是实际有效的

--- src/shuo/laoma/concurrent/c69/InterruptReadDemo.java
+++ src/shuo/laoma/concurrent/c69/InterruptReadDemo.java
@@ -1,14 +1,22 @@
 package shuo.laoma.concurrent.c69;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 
 public class InterruptReadDemo {
-    private static class A extends Thread {
-        @Override
+	
+    private class A extends Thread {
+    	private FileInputStream in;
+    	
+    	public A(FileInputStream in){
+    		this.in=in;
+    	}
+        
+    	@Override
         public void run() {
             while (!Thread.currentThread().isInterrupted()) {
                 try {
-                    System.out.println(System.in.read());
+                    System.out.println(in.read());
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
@@ -18,18 +26,18 @@ public class InterruptReadDemo {
 
         public void cancel() {
             try {
-                System.in.close();
+                in.close();
             } catch (IOException e) {
             }
             interrupt();
         }
     }
 
-    public static void main(String[] args) throws InterruptedException {
-        A t = new A();
+    public static void main(String[] args) throws InterruptedException, IOException {
+    	//need to set file name locally
+        A t = new InterruptReadDemo().new A(new FileInputStream("D:/CrackCaptcha.log"));
         t.start();
         Thread.sleep(100);
-
         t.cancel();
     }
 }
